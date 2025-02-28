5d32a6424088da9f6475de7b3c51350390ffbccc
okorion <ok.or.orion@gmail.com>
2025-02-28 18:52:31 +0900

    feat: 이미지 to gif

--- study_python_package.py
+++ study_python_package.py
@@ -7,3 +7,14 @@ png(jpg) to gif, pil, image
 import glob
 from PIL import Image
 
+# 이미지, 결과 생성 경로
+path_in = "./images/*.png"
+path_out = "./image_out/result.gif"
+
+# 첫 번째 이미지 & 모든 이미지 리스트 팩킹
+img, *imgs = [Image.open(f) for f in sorted(glob.glob(path_in))]
+
+# 이미지 저장
+img.save(
+    fp=path_out, format="GIF", append_images=imgs, save_all=True, duration=500, loop=0
+)
